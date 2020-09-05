b842c7c703c488bb3db5458d3fd67f77e82d3cd0
kemylly <kemylly@users.noreply.github.com>
2020-09-05 00:54:05 -0300

    Adicionei o sln e arquivos js

---
+++ aula_javascript_dio/aula_javascript_dio/js/main.js
@@ -0,0 +1,141 @@
+﻿/*var nome = "Kemylly cavalcante";
+var idade = 18;
+var idade2 = 10;
+var frase = "Golden é o melhor time";
+//var idade = "18";
+//var idade2 = "10";
+
+//alert("Bem vinda " + nome);
+
+//alert(nome + " tem " + idade + " anos");
+
+alert(idade + idade2);
+
+//cosole aparece no console do navegador, inspecionar
+console.log(nome);
+console.log(idade + idade2);
+
+console.log(frase.replace("Golden", "Bulls"));
+alert(frase.replace("Golden", "Lakers"));
+
+//tudo minusculo
+//console.log(frase.toLowerCase());
+
+//tudo maiusculo
+console.log(frase.toUpperCase());
+
+//contas
+alert(idade * idade2);
+
+alert(idade / idade2);
+
+alert(idade - idade2); */
+
+
+
+//array
+
+/*
+var lista = ["maca", "pera", "laranja"];
+
+//push é para por
+lista.push("uva");
+
+//pop é para tirar
+//lista.pop("");
+
+//console.log(lista[1]);
+
+//conta quantidade
+//console.log(lista.length);
+
+//traz os elementos contrario
+//console.log(lista.reverse());
+
+//console.log(lista);
+
+//alert(lista[1]);
+
+console.log(lista[0]);  //cada posicao do array
+console.log(lista.toString());  //transforma o array em string
+console.log(lista.toString()[0]); //traz so a primrira letra
+console.log(lista.join(" - ")); //troca a separacao de virgula por alguma coisa
+*/
+
+//propriedades 
+/*
+var fruta = { nome: "maca", cor: "vermelho" }
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//com dois array 
+/*
+var frutas = [{ nome: "maca", cor: "vermelho" }, { nome: "uva", cor: "roxo" }]
+console.log(frutas);
+//console.log(frutas.nome);
+alert(frutas[1].nome); */
+
+/*
+//condicao if e else
+//var idade = 18;
+var idade = prompt("qual sua idade"); //faz uma pergunta
+if (idade >= 18) {
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+};
+*/
+
+//laco de repeticao while
+/*var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    // count = count + 1;
+    count++;
+};*/
+
+
+//laco de repeticao for
+/*var count;
+for (count = 0; count <= 5; count++) {
+    alert(count)
+};*/
+
+
+//datas
+/*var d = new Date();
+alert(d); //data generalizada  */
+
+var d = new Date();
+alert(d.getDate());
+alert(d.getMonth());
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
